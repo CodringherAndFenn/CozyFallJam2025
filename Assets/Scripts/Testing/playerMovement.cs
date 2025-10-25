c885276cf9533838b22ee8a3696b509014091442
vluvl <vladmuscoi@gmail.com>
2025-10-25 10:48:42 +0200

    Temp scripts for testing things

--- Assets/Scripts/Testing/playerMovement.cs
+++ Assets/Scripts/Testing/playerMovement.cs
@@ -5,18 +5,37 @@ public class playerMovement : MonoBehaviour
 {
     private Rigidbody rb;
     [SerializeField] private float speed = 10;
+	public Transform orientation;
+	private float horizontalIn;
+	private float verticalIn;
+
+	private Vector3 moveDir;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+		rb.freezeRotation = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontalIn = Input.GetAxisRaw("Horizontal") * speed;
-        float verticalIn = Input.GetAxisRaw("Vertical") * speed;
+        horizontalIn = Input.GetAxisRaw("Horizontal") * speed;
+        verticalIn = Input.GetAxisRaw("Vertical") * speed;
         
-        rb.linearVelocity = new Vector3(horizontalIn, 0 , verticalIn);
+        //rb.linearVelocity = new Vector3(horizontalIn, 0 , verticalIn);
+        
     }
+
+    void FixedUpdate()
+    {
+	    move();
+    }
+    
+	private void move()
+	{
+		moveDir = orientation.forward * verticalIn + orientation.right * horizontalIn;
+		rb.AddForce(moveDir.normalized * speed, ForceMode.Force);
+	}
+
 }
